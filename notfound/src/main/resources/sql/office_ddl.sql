0d01c1b7192f3e5a5a0b6bb918bfb9f358fe3ec4
1234Juser <1211shjung@gmail.com>
2025-03-12 12:33:36 +0900

    [Docs] DB/회원 DDL 수정

--- notfound/src/main/resources/sql/office_ddl.sql
+++ notfound/src/main/resources/sql/office_ddl.sql
@@ -1,31 +1,3 @@
--- --------------------------------------------------------
--- db 생성 및 유저 권한 할당
--- --------------------------------------------------------
-/*-- 1) 새로운 officenotfound 계정 만들기
-create user 'officenotfound'@'%' identified by 'officenotfound';
--- 'localhost'대신 '%'(와일드카드 패턴)를 사용하면 외부 IP에서도 접근 가능하다.
-
--- 현재 존재하는 데이터베이스 확인
-show databases;
-
--- mysql 데이터베이스로 이동.
-use mysql;
-
-select * from user;
-select User from user;
-
--- 2) 데이터베이스 생성 후 계정에 권한 부여
--- officedb라는 이름으로 데이터베이스(=스키마) 생성.
-create database officedb;
-
--- 왼쪽 Navigator를 새로고침해서 officedb database(schema)가 추가된 것을 확인한다.
--- MySQL은 개념적으로 database와 schema를 구분하지 않는다.
--- (CREATE DATABASE와 CREATE SCHEMA가 같은 개념이다.)
-
--- officedb 스키마 및 하위에 대해 모든 권한을 부여
-grant all privileges on officedb.* to 'officenotfound'@'%';
-
-show grants for 'officenotfound'@'%';
 -- use officedb
 
 -- 1) 테이블 생성
@@ -45,7 +17,7 @@ CREATE TABLE IF NOT EXISTS tbl_member
     member_password VARCHAR(255) NOT NULL COMMENT '비밀번호',
     member_email VARCHAR (20) NOT NUll COMMENT '이메일',
     member_phone VARCHAR (20) NOT NULL COMMENT '전화번호',
-    member_enddate VARCHAR (30) NUll COMMENT '탈퇴날짜',
+    member_enddate DATETIME NUll COMMENT '탈퇴날짜',
     member_endstatus VARCHAR (20) NOT NULL DEFAULT 'N' COMMENT '탈퇴여부',
     -- TABLE LEVEL CONSTRAINTS
     CONSTRAINT pk_category_code PRIMARY KEY (member_code)
@@ -74,15 +46,3 @@ CREATE TABLE IF NOT EXISTS tbl_member_role
     CONSTRAINT fk_member_code FOREIGN KEY (member_code) REFERENCES tbl_member (member_code),
     CONSTRAINT fk_authority_code FOREIGN KEY (authority_code) REFERENCES tbl_authority (authority_code)
     ) ENGINE=INNODB COMMENT '회원별권한';
-
-
-
-
-
-
--- officedb로 이동
-use officedb;*/
-
--- --------------------------------------------------------
--- ddl
--- -------------------------------------------------------
