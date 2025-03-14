4039c87b98e6fd5fd3c1ccc4b966df35d82130d2
1234Juser <1211shjung@gmail.com>
2025-03-14 16:29:30 +0900

    [feat] 회원가입 기능 구현 추가
    - 입력 안하고도 회원가입 방지 기능 추가

--- notfound/src/main/resources/sql/office_db.sql
+++ notfound/src/main/resources/sql/office_db.sql
@@ -9,13 +9,13 @@ CREATE DATABASE officedb;
 SHOW DATABASES;
 
 -- 3) 유저 생성 (notfound/notfound)
-CREATE USER 'notfound'@'%' IDENTIFIED BY 'notfound';
+CREATE USER 'officenotfound'@'%' IDENTIFIED BY 'officenotfound';
 SELECT * FROM user;
 
 
 -- 4) 유저에게 권한 부여
-GRANT ALL PRIVILEGES ON officedb.* TO 'notfound'@'%';
-SHOW GRANTS FOR 'notfound'@'%';
+GRANT ALL PRIVILEGES ON officedb.* TO 'officenotfound'@'%';
+SHOW GRANTS FOR 'officenotfound'@'%';
 
 
 -- 5) SQL을 실행할 타겟 스키마(officedb)로 이동
