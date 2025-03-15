a41c5bcf7874d1bf062b192ea3d8267816763a42
1234Juser <1211shjung@gmail.com>
2025-03-15 18:29:15 +0900

    [feat] 로그인 기능 구현 중

--- notfound/src/main/java/com/office/notfound/member/model/dto/AuthorityDTO.java
+++ notfound/src/main/java/com/office/notfound/member/model/dto/AuthorityDTO.java
@@ -1,4 +1,38 @@
 package com.office.notfound.member.model.dto;
 
 public class AuthorityDTO {
+    private int authorityCode;
+    private String authorityName;
+
+    public AuthorityDTO() {
+    }
+
+    public AuthorityDTO(int authorityCode, String authorityName) {
+        this.authorityCode = authorityCode;
+        this.authorityName = authorityName;
+    }
+
+    public int getAuthorityCode() {
+        return authorityCode;
+    }
+
+    public void setAuthorityCode(int authorityCode) {
+        this.authorityCode = authorityCode;
+    }
+
+    public String getAuthorityName() {
+        return authorityName;
+    }
+
+    public void setAuthorityName(String authorityName) {
+        this.authorityName = authorityName;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorityDTO{" +
+                "authorityCode=" + authorityCode +
+                ", authorityName='" + authorityName + '\'' +
+                '}';
+    }
 }
--- notfound/src/main/java/com/office/notfound/member/model/dto/MemberDTO.java
+++ notfound/src/main/java/com/office/notfound/member/model/dto/MemberDTO.java
@@ -1,52 +1,85 @@
 package com.office.notfound.member.model.dto;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class MemberDTO implements UserDetails {
 
-    private int memberCode;
-    private String memberName;
-    private String memberId;
-    private String memberPassword;
-    private String memberEmail;
-    private String memberPhone;
-    private LocalDateTime memberEnddate = null;  // 탈퇴 날짜 기본값 null
-    private String memberEndstatus = "N";  // 기본 탈퇴 상태 'N'
-    private List<AuthorityDTO> memberAuthorities;
+    private int memberCode;               // 회원번호
+    private String memberId;              // 사용자 ID
+    private String memberPassword;        // 비밀번호
+    private String memberName;            // 이름
+    private String memberEmail;           // 이메일
+    private String memberPhone;           // 전화번호
+    private String memberEndstatus;       // 탈퇴여부
+    private LocalDateTime memberEnddate;  // 탈퇴 날짜
+    private List<AuthorityDTO> memberAuthorities;  // 권한 정보
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+        this.memberAuthorities.forEach(authority ->
+                authorities.add(new SimpleGrantedAuthority(authority.getAuthorityName())));
+
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return "";
+        return this.memberPassword;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return this.memberId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    /* 설명.
+     *  계정 비활성화 여부로 사용자가 사용할 수 없는 상태를 나타내며,
+     *  false면 Spring Security가 인증 작업을 수행해주지 않아 계정을 사용할 수 없다.
+     *  (디폴트는 true)
+     * */
+    @Override
+    public boolean isEnabled() {
+        return !"Y".equals(memberEndstatus); // 탈퇴 상태가 'Y'이면 false 반환
     }
 
     public MemberDTO() {
     }
 
-    public MemberDTO(int memberCode, String memberName, String memberId, String memberPassword, String memberEmail,
-                     String memberPhone, LocalDateTime memberEnddate, String memberEndstatus, List<AuthorityDTO> memberAuthorities) {
+    public MemberDTO(int memberCode, String memberId, String memberPassword, String memberName, String memberEmail, String memberPhone,
+                     String memberEndstatus, LocalDateTime memberEnddate, List<AuthorityDTO> memberAuthorities) {
         this.memberCode = memberCode;
-        this.memberName = memberName;
         this.memberId = memberId;
         this.memberPassword = memberPassword;
+        this.memberName = memberName;
         this.memberEmail = memberEmail;
         this.memberPhone = memberPhone;
-        this.memberEnddate = memberEnddate;
         this.memberEndstatus = memberEndstatus;
+        this.memberEnddate = memberEnddate;
         this.memberAuthorities = memberAuthorities;
     }
 
@@ -56,14 +89,6 @@ public class MemberDTO implements UserDetails {
 
     public void setMemberCode(int memberCode) {
         this.memberCode = memberCode;
-    }
-
-    public String getMemberName() {
-        return memberName;
-    }
-
-    public void setMemberName(String memberName) {
-        this.memberName = memberName;
     }
 
     public String getMemberId() {
@@ -82,6 +107,14 @@ public class MemberDTO implements UserDetails {
         this.memberPassword = memberPassword;
     }
 
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
     public String getMemberEmail() {
         return memberEmail;
     }
@@ -98,20 +131,20 @@ public class MemberDTO implements UserDetails {
         this.memberPhone = memberPhone;
     }
 
-    public LocalDateTime getMemberEnddate() {
-        return memberEnddate;
-    }
-
-    public void setMemberEnddate(LocalDateTime memberEnddate) {
-        this.memberEnddate = memberEnddate;
-    }
-
     public String getMemberEndstatus() {
         return memberEndstatus;
     }
 
     public void setMemberEndstatus(String memberEndstatus) {
         this.memberEndstatus = memberEndstatus;
+    }
+
+    public LocalDateTime getMemberEnddate() {
+        return memberEnddate;
+    }
+
+    public void setMemberEnddate(LocalDateTime memberEnddate) {
+        this.memberEnddate = memberEnddate;
     }
 
     public List<AuthorityDTO> getMemberAuthorities() {
@@ -126,16 +159,14 @@ public class MemberDTO implements UserDetails {
     public String toString() {
         return "MemberDTO{" +
                 "memberCode=" + memberCode +
-                ", memberName='" + memberName + '\'' +
                 ", memberId='" + memberId + '\'' +
                 ", memberPassword='" + memberPassword + '\'' +
+                ", memberName='" + memberName + '\'' +
                 ", memberEmail='" + memberEmail + '\'' +
                 ", memberPhone='" + memberPhone + '\'' +
-                ", memberEnddate=" + memberEnddate +
                 ", memberEndstatus='" + memberEndstatus + '\'' +
+                ", memberEnddate=" + memberEnddate +
                 ", memberAuthorities=" + memberAuthorities +
                 '}';
     }
-
-
 }
