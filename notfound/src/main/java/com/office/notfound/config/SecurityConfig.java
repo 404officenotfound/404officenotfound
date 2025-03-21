eb5c719df481dfc8a65119338dc029a7781fcae0
1234Juser <1211shjung@gmail.com>
2025-03-21 10:43:34 +0900

    [feat] SecurityConfig 수정

--- notfound/src/main/java/com/office/notfound/config/SecurityConfig.java
+++ notfound/src/main/java/com/office/notfound/config/SecurityConfig.java
@@ -43,7 +43,9 @@ public class SecurityConfig {
         // #1. 서버의 리소스에 접근 가능한 권한을 설정
         http.authorizeHttpRequests(auth -> {
             // 로그인, 회원가입, 실패 페이지(authFailHandler정의)와 홈(home)은 모두에게 접근 허용
-            auth.requestMatchers("/auth/login","/member/signup","/main", "/", "/member/check-duplicate").permitAll();
+            auth.requestMatchers("/auth/login","/member/signup","/main", "/", "/member/check-duplicate", "/member/find-id",
+                    "/member/reset-password", "/company/introduce", "/company/ceo", "/store/storelist", "/store/storeregion",
+                    "/faq/list", "/event/list", "/store/detailstore/*").permitAll();
             // '/admin/*' 엔드포인트는 ADMIN 권한을 가진 사용자만 접근 허용
             auth.requestMatchers("/admin/*").hasAnyAuthority("ADMIN");
             // '/user/*' 엔드포인트는 USER 권한을 가진 사용자만 접근 허용
